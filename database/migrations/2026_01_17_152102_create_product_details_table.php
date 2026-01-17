ea84f356543fa38392dc2d9f679034c3c1326c04
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-17 19:51:17 +0430

    new code

--- database/migrations/2026_01_17_152102_create_product_details_table.php
+++ database/migrations/2026_01_17_152102_create_product_details_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('product_details', function (Blueprint $table) {
             $table->id();
-            $table->string("name")->unique();
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('product_details');
     }
 };
