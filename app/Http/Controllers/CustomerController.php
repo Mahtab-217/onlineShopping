b86e611cc0c1098b39d1c4cd63457744c48ccc79
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-17 20:07:43 +0430

    new view

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
     //
+    public function index(){
+       $users= User::where('user_type',"customer")->get();
+        return view('Customer.home',["users"=>$users]);
+    }
 }
--- resources/views/Customer/home.blade.php
+++ resources/views/Customer/home.blade.php
@@ -5,18 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
+<body class="w-full max-w-6xl mx-auto mt-2">
    
-    <div>
+    <div class="flex flex-col gap-4">
  <h1>All our Customers</h1>
- <table>
+ <table class="text-center border border-collapse">
     <tr>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Last Name</th>
-        <th>Email</th>
-        <th>Gender</th>
+        <th class="border py-2 px-4">Id</th>
+        <th class="border py-2 px-4">Name</th>
+        <th class="border py-2 px-4">Last Name</th>
+        <th class="border py-2 px-4">Email</th>
+        <th class="border py-2 px-4">Gender</th>
     </tr>
  </table>
     </div>
