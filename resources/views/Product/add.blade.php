475d43ae2e3b6b1f223b76fca3bfa6570d604e9d
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-20 21:24:33 +0430

    updated codes

---
+++ resources/views/Product/add.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="w-full bg-gray-200 h-screen p-3 justify-center items-center flex ">
+        <div class="border rounded-sm p-6 w-8/12 lg-7/12 md-8/12">
+            <h1 class="text-center text-bold text-2xl ">Add new product</h1>
+               <form action="" method="POST" class="flex p-4 flex-col gap-3 w-full">
+                @csrf
+                <input type="text" name="name" placeholder="Producr name" class="border focus:outline-none focus:border-none focus:ring ring-indigo-600 rounded-sm p-2 ">
+                <input type="number" name="price" placeholder="Producr price" class="border focus:outline-none focus:border-none focus:ring ring-indigo-600 rounded-sm p-2 ">
+                <input type="number" name="quantity" placeholder="Producr quantity" class="border focus:outline-none focus:border-none focus:ring ring-indigo-600 rounded-sm p-2 ">
+                <input type="text" name="madein" placeholder="Made in a country?" class="border focus:outline-none focus:border-none focus:ring ring-indigo-600 rounded-sm p-2 ">
+                <input type="file" accept="image/*" name="photo" class="border focus:outline-none focus:border-none focus:ring ring-indigo-600 rounded-sm p-2 ">
+               </form>
+        </div>
+    </div>
+</body>
+</html>
